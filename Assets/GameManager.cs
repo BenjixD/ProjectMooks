6a1e232cbb0ff0a63379b9be539af459599a7b30
Yilun Zhao <yl4zhao@uwaterloo.ca>
2020-05-04 18:19:36 -0400

    Adding party system

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,31 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager> {
-	public PlayerQueue playerQueue;
 
-	List<PlayerCreationData> GetNPlayers(int n) {
-		List<PlayerCreationData> players = new List<PlayerCreationData>();
-		for(int i = 0; i < n; i++) {
-			PlayerCreationData data = playerQueue.Dequeue();
-			if(data != null) {
-				players.Add(data);
-			}
-		}
-		return players;
-	}
-
-	void Start() {
-		StartCoroutine(TestPlayers());
-	}
-
-	IEnumerator TestPlayers() {
-		List<PlayerCreationData> players = GetNPlayers(2);
-		for(int i = 0; i < players.Count; i++) {
-			Debug.Log("Deploying Player: " + players[i].name);
-			players[i].stats.LogStats();
-			playerQueue.Remove(players[i].name);
-		}
-		yield return new WaitForSeconds(5f);
-		yield return StartCoroutine(TestPlayers());
-	}
 }
---
+++ Assets/_Scripts/Party.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Party : MonoBehaviour {
+    public PlayerQueue playerQueue;
+    public GameObject playerTemplate;
+
+    // (Name, (position, player obj)) mapping
+    private Dictionary<string, Tuple<int, Player>> players = new Dictionary<string, Tuple<int, Player>>();
+    [SerializeField]
+    private string[] playerPos = new string[3];
+
+    List<PlayerCreationData> GetNPlayers(int n) {
+        List<PlayerCreationData> players = new List<PlayerCreationData>();
+        for(int i = 0; i < n; i++) {
+            PlayerCreationData data = playerQueue.Dequeue();
+            if(data != null) {
+                players.Add(data);
+            }
+        }
+        return players;
+    }
+
+    Player CreatePlayer(PlayerCreationData data) {
+        Player player = Instantiate(playerTemplate).GetComponent<Player>();
+        player.stats = data.stats;
+        return player;
+    }
+
+    void EvictPlayer(string username) {
+        Tuple<int, Player> player = players[username];
+        playerPos[player.Item1] = null;
+        players.Remove(username);
+    }
+
+    void TryFillPartySlot(int index) {
+        if(playerPos[index] != null) {
+            return;
+        } else {
+            PlayerCreationData data = GetNPlayers(1)[0];
+            if(data != null) {
+                Player p = CreatePlayer(data);
+                playerPos[index] = data.name;
+                players.Add(data.name, new Tuple<int, Player>(index, p));
+            }
+        }
+    }
+
+    void TryFillAllPartySlots() {
+        for(int i = 0; i < playerPos.Length; i++) {
+            TryFillPartySlot(i);
+        }
+    }
+
+    void Start() {
+        StartCoroutine(TestPlayers());
+    }
+
+    IEnumerator TestPlayers() {
+        List<PlayerCreationData> players = GetNPlayers(2);
+        for(int i = 0; i < players.Count; i++) {
+            Debug.Log("Deploying Player: " + players[i].name);
+            players[i].stats.LogStats();
+            playerQueue.Remove(players[i].name);
+        }
+        yield return new WaitForSeconds(5f);
+        yield return StartCoroutine(TestPlayers());
+    }
+}
