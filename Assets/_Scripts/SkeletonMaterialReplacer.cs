9c925b82cf65500197f4debe76321e9cd98b995c
Anthony Tu <antu3199@gmail.com>
2020-05-07 15:51:28 -0400

    Outlines working

---
+++ Assets/_Scripts/SkeletonMaterialReplacer.cs
@@ -0,0 +1,51 @@
+
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class SkeletonMaterialReplacer : MonoBehaviour {
+
+   private Material[] materialsIWantToUse;
+   private MeshRenderer meshRenderer;
+
+    // Spine sets the material every frame, so we need to hack it to change material properties at runtime:
+    // http://esotericsoftware.com/forum/Different-materials-for-same-Spine-character-5445
+
+    public bool setOriginal = false;
+
+   void Awake() {
+       meshRenderer = GetComponent<MeshRenderer>();
+   }
+
+   void OnWillRenderObject ()
+   {
+        if (materialsIWantToUse == null || meshRenderer == null || setOriginal == true) {
+            return;
+        }
+
+       // Use this for multi-material setups.
+        var sharedMaterials = meshRenderer.sharedMaterials;
+
+        if (materialsIWantToUse.Length != sharedMaterials.Length) {
+            Debug.LogError("Incorrect amount of materials");
+            return;
+        }
+
+        for (int i = 0; i < sharedMaterials.Length; i++) {
+            sharedMaterials[i] = materialsIWantToUse[i];
+        }
+        meshRenderer.sharedMaterials = sharedMaterials;
+   }
+
+   public void SetMaterialOverride(Material[] materialsIWantToUse) {
+       this.materialsIWantToUse = materialsIWantToUse;
+   }
+
+    public void UseMaterialOverride() {
+        this.setOriginal = false;
+    }
+
+   public void UseOriginal() {
+       this.setOriginal = true;
+   }
+}
--- Assets/_Scripts/TargetSelectionUI.cs
+++ Assets/_Scripts/TargetSelectionUI.cs
@@ -10,17 +10,22 @@ public class TargetSelectionUI : MonoBehaviour
 {
 
     public Color targetSelectionColor;
+    public float targetSelectionOutlineWidth = 3.5f;
+
 
     private List<FightingEntity> potentialTargets;
     private CommandSelector _commandSelector {get; set;}
 
-
+    List<Material[]> outlinedMaterials;
 
     
     public void InitializeTargetSelectionSingle(List<FightingEntity> possibleTargets, int initialChoice, CommandSelector selector) {
         this.potentialTargets = possibleTargets;
         this._commandSelector = selector;
         this._commandSelector.Initialize(0, possibleTargets.Count-1, this.UpdateTargetSelectionUI, true);
+
+        this.CreateOutlinedMaterials(possibleTargets);
+
         this.UpdateTargetSelectionUI();
     }
 
@@ -30,41 +35,62 @@ public class TargetSelectionUI : MonoBehaviour
     }
 
     public void ClearSelection() {
-        //foreach (var target in potentialTargets) {
-        //    target.GetComponent<SpriteRenderer>().color = Color.white;
-        //}
+        for (int i = 0; i < potentialTargets.Count; i++) {
+            this.SetMaterialOutline(i, false);
+        }
 
         potentialTargets.Clear();
         _commandSelector = null;
     }
 
+    private void CreateOutlinedMaterials(List<FightingEntity> possibleTargets) {
+        outlinedMaterials = new List<Material[]>();
+        foreach (var target in possibleTargets) {
+            Material[] materials = target.GetComponent<MeshRenderer>().materials;
+            foreach (var material in materials) {
+                material.SetFloat("_OutlineWidth", targetSelectionOutlineWidth);
+                material.SetColor("_OutlineColor", targetSelectionColor);
+            }
+            outlinedMaterials.Add(materials);
+        }
+    }
+
     private void UpdateTargetSelectionUI() {
         // TOOD: FIX sprite highlighting
 
-        return;
 
         if (potentialTargets == null) {
             return;
         }
 
-        foreach (var target in potentialTargets) {
-            target.GetComponent<SpriteRenderer>().color = Color.white;
+        for (int i = 0; i < potentialTargets.Count; i++) {
+            this.SetMaterialOutline(i, false);
         }
 
         if (this._commandSelector != null) {
             // Single target
-            FightingEntity target = potentialTargets[_commandSelector.GetChoice()];
-            target.GetComponent<SpriteRenderer>().color = targetSelectionColor;
+            int index = _commandSelector.GetChoice();
+            FightingEntity target = potentialTargets[index];
+            this.SetMaterialOutline(index, true);
 
         } else {
             // All targets
-            foreach (var target in potentialTargets) {
-                target.GetComponent<SpriteRenderer>().color = targetSelectionColor;
+            for (int i = 0; i < potentialTargets.Count; i++) {
+                this.SetMaterialOutline(i, true);
             }
         }
     }
 
+    private void SetMaterialOutline(int targetIndex, bool enabled) {
+        FightingEntity target = potentialTargets[targetIndex];
+        SkeletonMaterialReplacer materialReplacer = target.GetComponent<SkeletonMaterialReplacer>();
+        if (enabled) {
+            materialReplacer.UseMaterialOverride();
+            materialReplacer.SetMaterialOverride(outlinedMaterials[targetIndex]);
+        } else {
+            materialReplacer.UseOriginal();
+        }
 
-
+    }
 
 }
