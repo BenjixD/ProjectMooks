bf7603525c348384fa7fe9a44bed3fca9e5f9345
Leo <lileo.jh@gmail.com>
2020-05-29 02:48:47 -0400

    Minor bug fixes: missing refs and unupdated actions

--- Assets/_Scripts/Actions/ThunderOneTest.cs
+++ Assets/_Scripts/Actions/ThunderOneTest.cs
@@ -8,8 +8,7 @@ public class ThunderOneTest : ActionBase {
     private List<StatusAilment> _status;
 
     public override bool TryChooseAction(FightingEntity user, string[] splitCommand) {
-        // Fire One command format: !m fire [target number]
-        if (!BasicValidation(splitCommand)) {
+        if (!base.TryChooseAction(user, splitCommand)) {
             return false;
         }
 
