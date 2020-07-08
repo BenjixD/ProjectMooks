7da5aa4b8026604fc2a9f632c5e94482708af348
Leo <lileo.jh@gmail.com>
2020-07-08 15:03:10 -0400

    Chatboxes rescaled

--- Assets/_Scripts/UI/WorldspaceToScreenUI.cs
+++ Assets/_Scripts/UI/WorldspaceToScreenUI.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class WorldspaceToScreenUI : MonoBehaviour {
     [Tooltip("The transform to keep properly positioned.")]
     [SerializeField] private Transform _uiTransform;
+    [Tooltip("Optional transform to set the world point to.")]
+    [SerializeField] private Transform _worldPointTransform;
     private Vector3 _worldPoint;
     private Camera _cam;
 
     private void Awake() {
         _cam = Camera.main;
+        if (_worldPointTransform != null) {
+            _worldPoint = _worldPointTransform.position;
+        }
         UpdatePosition();
     }
 
     private void Update() {
         UpdatePosition();
     }
+
 
     public void SetWorldPoint(Vector3 point) {
         _worldPoint = point;
