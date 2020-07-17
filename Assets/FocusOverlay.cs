65aa2e30b229c8ecf19f6c069d965f538e515b6e
Anthony Tu <antu3199@gmail.com>
2020-07-17 00:12:35 -0400

    Focus overlay start

---
+++ Assets/FocusOverlay.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FocusOverlay : MonoBehaviour
+{
+
+    const string FOCUS_LAYER_NAME = "Focus Layer";
+
+    private Dictionary<GameObject, int> previousLayers;
+
+    public void EnableOverlay() {
+        this.gameObject.SetActive(true);
+    }
+
+    public void DisableOverlay() {
+        this.UnsetAllFocusOverlays(); 
+        this.gameObject.SetActive(false);
+    }
+
+    public void SetFocusLayer(GameObject obj, bool setChildren = false) {
+        if (previousLayers.ContainsKey(obj)) {
+            return;
+        }
+
+        previousLayers[obj] = obj.layer;
+        this.RecSetFocusLayer(obj, setChildren);
+    }
+
+    public void RecSetFocusLayer(GameObject obj, bool setChildren) {
+        obj.layer = LayerMask.GetMask(FOCUS_LAYER_NAME);
+
+        if (!setChildren) {
+            return;
+        }
+
+        foreach (Transform child in obj.transform) {
+            RecSetFocusLayer(child.gameObject, setChildren);
+        }
+    }
+
+    private void UnsetAllFocusOverlays() {
+        foreach (KeyValuePair<GameObject, int> pair in previousLayers) {
+            UnsetFocusLayer(pair.Key, false); // true might have unintended side effects
+        }
+    }
+
+    private void UnsetFocusLayer(GameObject obj, bool setChildren = false) {
+        if (!previousLayers.ContainsKey(obj)) {
+            return;
+        }
+
+        RecUnsetFocusLayer(obj, previousLayers[obj], setChildren);
+
+
+        previousLayers.Remove(obj);
+    }
+
+    private void RecUnsetFocusLayer(GameObject obj, int layer, bool setChildren) {
+        obj.layer = layer;
+
+        if (!setChildren) {
+            return;
+        }
+
+        foreach (Transform child in obj.transform) {
+            RecUnsetFocusLayer(child.gameObject, layer, setChildren);
+        }
+    }
+
+}
